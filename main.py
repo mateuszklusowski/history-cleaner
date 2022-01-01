32865bbd39ee42e6dfb1689ac892abe039780c57
Mateusz Kłusowski <mateuszk@pc3.home>
2022-01-01 21:39:25 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import sqlite3
+
+
+def delete(site) -> str:
+
+    with sqlite3.connect('YOUR GOOGLE PROGILE HISTORY FILE PATH') as conn:
+        c = conn.cursor()
+
+        while True:
+            ids = []
+
+            for row in c.execute(f'SELECT id, url FROM urls WHERE url LIKE "%{site}%"'):
+                print(row)
+                id = row[0]
+                ids.append((id,))
+            
+            c.executemany('DELETE FROM urls WHERE id=?', ids)
+            conn.commit()
+            break
+    
+
+delete("YOUR SITE")
